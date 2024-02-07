c8820159a168dced5af1839827f78d0f78ff714d
lubava <joheunsaram@mail.ru>
2024-02-07 23:45:06 +0300

    edit filters

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Filters from './components/Filters/Filters';
 function App() {
   return (
     <div className='App'>
+      <h1>ВАСИЛИСА, для того, чтобы увидеть данные в таблице</h1>
+      <h1>выбери только Цель и Канал - данные сразу отобразятся</h1>
       <Filters />
     </div>
   );
