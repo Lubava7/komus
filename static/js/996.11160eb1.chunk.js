5452e846e6a8451bd86a7eef8ecc34ce2e4b9f91
lubava <joheunsaram@mail.ru>
2024-02-21 18:59:31 +0300

    Updates

--- static/js/996.11160eb1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkomus=self.webpackChunkkomus||[]).push([[996],{2996:(s,e,t)=>{t.r(e),t.d(e,{default:()=>a});t(9060);var u=t(2496);const a=s=>{let{state:e}=s;return(0,u.jsx)("div",{children:"SalesList"})}}}]);
-//# sourceMappingURL=996.11160eb1.chunk.js.map
